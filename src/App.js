aca445dc536a195a40099fff6d89ee150db54f38
Malkith99 <malkithamanda@gmail.com>
2023-02-10 13:08:47 +0530

    Class vs Function Approaches

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import CounterCalss from './components/CounterClass';
+import CounterFunction from './components/CounterFunction';
 
-function App() {
+function App() {         
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hello React</h1>
+      <CounterCalss/>      {/* calling the counter class  */}   
+      <hr></hr> 
+      <CounterFunction/>              
+    </div> 
   );
 }
 
