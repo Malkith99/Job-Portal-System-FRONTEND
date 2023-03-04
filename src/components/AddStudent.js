37534a7f8091a5672126de37750622c8458a2776
Malkith99 <malkithamanda@gmail.com>
2023-03-05 00:03:20 +0530

    set methods are implemented

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -1,25 +1,38 @@
 import React,{useState} from "react";
 export default function AddStudent(){
+    const [name,setName]=useState("");
+    const [age,setAge]=useState("");
+    const [gender,setGender]=useState("");
+
     return(
         <div className="container">
             <form>
                 <div className="form-group">
                     <label for="name" className="form-label">Student Name</label>
-                    <input type="text" className="form-control" id="name" placeholder="Enter Student Name"></input>
+                    <input type="text" className="form-control" id="name" placeholder="Enter Student Name" 
+                    onChange={(e)=>{
+                        setName(e.target.value);
+                    }}></input>
                   { /*<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> */}
                 </div>
 
                 <div className="form-group">
                     <label for="age" className="form-label">Age</label>
-                    <input type="text" className="form-control" id="age" placeholder="Enter Student Age"></input>
+                    <input type="text" className="form-control" id="age" placeholder="Enter Student Age"
+                    onChange={(e)=>{
+                        setAge(e.target.value);
+                    }}></input>
                 </div>
 
                 <div className="form-group">
                     <label for="gender" className="form-label">Gender</label>
-                    <input type="text" className="form-control" id="gender" placeholder="Enter Student Gender"></input>
+                    <input type="text" className="form-control" id="gender" placeholder="Enter Student Gender"
+                    onChange={(e)=>{
+                    setGender(e.target.value);
+                    }}></input>
                 </div>
             
-            
+
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </div>
