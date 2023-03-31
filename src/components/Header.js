746528e351e5ffe8756b95dc26d7140dc5af4df3
Malkith99 <malkithamanda@gmail.com>
2023-03-31 21:37:22 +0530

    Home_Home

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ function Header() {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                 {/*<a className="nav-link active" aria-current="page" href="/">Home</a>*/}
-                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/" className="nav-link">Home_Home</Link>
                 </li>
                 <li className="nav-item">
                 {/*<a className="nav-link" href="/add">Create Student</a>   to shift new tab wothout refresh*/}
