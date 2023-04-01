ddf13a4f1e3d1d12a0c9e5ba5ee5aa496b7a3aad
Malkith99 <malkithamanda@gmail.com>
2023-04-01 22:46:33 +0530

    profile page created

---
+++ src/components/pages/student/profile/profile.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react';  //useEffect import fro reac hook and used in function based approach
+import axios from "axios";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
+
+export default function Profile(){
+    return(
+        <div className="container">            
+            <h1>PROFILE</h1>
+        </div>
+    )
+}
