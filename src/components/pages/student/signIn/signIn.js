0296367716555f1216538d3411393e052f52ae4c
Malkith99 <malkithamanda@gmail.com>
2023-04-01 22:46:53 +0530

    sign in page created

---
+++ src/components/pages/student/signIn/signIn.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { toast } from "react-toastify";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import axios from "axios";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+  
+  function StudentRegistrationBasic() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const handleEmailChange = (event) => {
+      setEmail(event.target.value);
+      console.log("email", email);
+    };
+  
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+      console.log("password", password);
+    };
+  
+  
+    const handleSubmit = (event) => {   // function that execute when pressing submit button"
+      console.log("Hello");
+      event.preventDefault();
+  
+      if (!email || !password || !confirmPassword) {
+        toast.error("Please fill in all fields");
+        return;
+      }
+  
+      // Add your registration logic here
+  
+  
+        const newStudent={
+            email,
+            password,
+        }
+       // console.log(newStudent);
+       axios.post("http://localhost:1234/student/add",newStudent ).then(()=>{
+        alert("Student Added")
+       }).catch((err)=>{
+        alert(err)
+       });
+  
+       toast.success("Successfully registered!");
+       setEmail("");
+       setPassword("");
+       setConfirmPassword("");
+                   
+    
+    } ;
+  
+  
+    return (
+      <div>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={1} align="center">
+            <Grid item xs={12}>
+              <Card
+                sx={{
+                  maxWidth: "50%",
+                  mt: 5,
+                  alignContent: "center",
+                  justifyItems: "flex-end",
+                }}
+              >
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 18, fontWeight: "bold" }}
+                    color="Black"
+                    gutterBottom
+                  >
+                    Step 1 : Registration
+                  </Typography>
+                  <form onSubmit={handleSubmit}>
+                    <TextField
+                      label="Email"
+                      variant="outlined"
+                      type="email"
+                      value={email}
+                      onChange={(e)=>{
+                        setEmail(e.target.value);
+                    }}
+                      fullWidth
+                      margin="normal"
+                    />
+                    <TextField
+                      label="Password"
+                      variant="outlined"
+                      type="password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                      fullWidth
+                      margin="normal"
+                    />
+                    
+                    {/* <Link to="/registerStudentUser"> */}
+                    { <Button variant="contained" color="primary" type="submit">
+                                          Sign In
+                                      </Button>}
+                    {/* </Link> */}
+                  </form>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+          <hr />
+          <Grid container spacing={2}>
+            <Grid
+              item
+              xs={12}
+              align="right"
+              mr={"25%"}
+              sx={{ alignItems: "flex-end" }}
+            >
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+    );
+  }
+  
+  export default StudentRegistrationBasic;
