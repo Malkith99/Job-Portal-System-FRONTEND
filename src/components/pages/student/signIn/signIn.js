ad961e9a1f68eda6f2ca651f22776b904a8ea33a
Malkith99 <malkithamanda@gmail.com>
2023-04-09 22:38:49 +0530

    changes on sigin In page

--- src/components/pages/student/signIn/signIn.js
+++ src/components/pages/student/signIn/signIn.js
@@ -11,14 +11,6 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import axios from "axios";
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  }));
   
   export default function StudentSignIn() {
     const [email, setEmail] = useState("");
