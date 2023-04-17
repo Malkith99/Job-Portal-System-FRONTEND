47308a612ca6cfe0c88bcaa8c39a3f1e60ab09a4
Malkith99 <malkithamanda@gmail.com>
2023-04-17 11:13:53 +0530

    Profile page is ongoing

--- src/components/pages/student/profile/profile.js
+++ src/components/pages/student/profile/profile.js
@@ -1,13 +1,29 @@
-import React,{useState,useEffect} from 'react';  //useEffect import fro reac hook and used in function based approach
+import './profile.css';
+import React,{useState,useEffect} from 'react';  //useEffect import from react hook and used in function based approach
 import axios from "axios";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 
 
 export default function Profile(){
-    return(
-        <div className="container">            
-            <h1>PROFILE</h1>
+
+    const [text, setText] = useState("");
+
+    function handleChange(event) {
+      setText(event.target.value);
+    }
+    return (
+    <div>
+    <h1>Profile</h1>
+        <div className='container'>
+      <div className="box">
+        <label htmlFor="textbox">Email: </label>
+        <div>
+        <input type="text" id="textbox" value={text} onChange={handleChange} />
         </div>
-    )
+        
+      </div>
+        </div>
+        </div>
+      );
 }
