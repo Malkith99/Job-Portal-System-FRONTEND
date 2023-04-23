c3b135d0a1321209bac0ee8a496308cad9586127
Malkith99 <malkithamanda@gmail.com>
2023-04-24 00:05:04 +0530

    bottom division of the page header is added

---
+++ src/components/mainHeader/headerBottom/headerBottom.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ProfileLogo from "../profileLogo/profileLogo";
+import "./headerBottom.css";
+
+function HeaderBottom(props) {
+  return (
+    <div class="header-bottom">
+      <div className="nav" style={{ height: "50px" }}>
+        <nav style={{ width: "100%" }}>
+          <div className="nav-bar-content" style={{ alignItems: "center" }}>
+            <div className="nav-bar-left">{props.content}</div>
+            <div className="nav-bar-right">
+              {props.isLogedIn ? (
+                <span className="ml-auto">
+                  <ProfileLogo onLogout={props.onLogout} />
+                </span>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </nav>
+      </div>
+      <p></p>
+    </div>
+  );
+}
+export default HeaderBottom;
