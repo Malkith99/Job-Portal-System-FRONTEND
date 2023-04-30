3a4b39dbbfce68645c2c55350403539817852c52
Malkith99 <malkithamanda@gmail.com>
2023-04-30 09:06:33 +0530

    Registration step 2 is created

---
+++ src/components/pages/student/studentRegistration/step2/UserDetailsMain.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Footer from "../../../../footer/footer";
+import MainHeader from "../../../../mainHeader/mainHeader";
+import UserDetails from "./userDetails/UserDetails";
+import "./UserDetailsMain.css";
+
+function UserDetailsMain({ isLogedIn, onLogout }) {
+  const content = (
+    <>
+      <Link to="/student-register">Student Registration</Link>
+      <Link to="/student-register">Step 1</Link>
+      <Link to="/student-register-final">Step 2</Link>
+    </>
+  );
+  return (
+    <>
+      <div>
+        <MainHeader
+          content={content}
+          isLogedIn={isLogedIn}
+          onLogout={onLogout}
+        />
+        <UserDetails />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default UserDetailsMain;
