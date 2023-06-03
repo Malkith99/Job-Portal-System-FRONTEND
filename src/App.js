9259318b1040dd656dc3fac80cc20797ee156bd9
Malkith99 <malkithamanda@gmail.com>
2023-06-03 10:44:45 +0530

    new paths are added

--- src/App.js
+++ src/App.js
@@ -13,15 +13,21 @@ import StudentSignIn from './components/pages/student/signIn/signIn';
 import StudentHome from './components/pages/student/home/StudentHome';
 import StudentRegistrationStep_2 from './components/pages/student/studentRegistration/step2/UserDetailsMain';
 import StudentAppliation from './components/pages/student/MyApplications/MyApplications';
-
-
+import CompanySignUp from './components/pages/company/companySignup/companySignup';
+import CompanySignIn from './components/pages/company/companyLogin/companyLogin';
+import CompanyHome from './components/pages/company/home/CompanyMainHome';
+import CompanyProfile from './components/pages/company/companyRegistration/CompanyRegisterMain';
+import CompanyJobPool from './components/pages/company/home/companyHome/CompanyJobPool/CompanyJobPool';
+import AllResponses from './components/pages/company/studentResponces/allResponces'; 
+/* import StudentApplicatinCompany from './components/pages/' ; */
+import profileImage from './components/pages/student/profileImage/profileImageMain';
 function App() {
-/*   const [isLogedIn, setIsLogedIn] = useState(true);
+/*    const [isLogedIn, setIsLogedIn] = useState(true);
 
   const handlLogOut = () => {
     setIsLogedIn(false);
-  };
- */
+  }; */
+ 
   return (
 
     <BrowserRouter>
@@ -34,7 +40,15 @@ function App() {
           <Route path="/student-signIn" element={<StudentSignIn/> }> </Route>
           <Route path="/student-profile" element={<Profile/> }> </Route>
           <Route path="/student-home" element={<StudentHome/> }> </Route>
-          <Route path="/student-application" element={<StudentAppliation/> }> </Route>
+          <Route path="/student-applications" element={<StudentAppliation/> }> </Route>
+          <Route path="/company-signup" element={<CompanySignUp/> }> </Route>
+          <Route path="/company-signin" element={<CompanySignIn/> }> </Route>
+          <Route path="/company-home" element={<CompanyHome/> }> </Route>
+          <Route path="/company-profile" element={<CompanyProfile/> }> </Route>
+          <Route path="/company-job-pool" element={<CompanyJobPool/> }> </Route>
+          <Route path="/all-student-responces" element={<AllResponses/> }> </Route>
+          <Route path="/profileImage" element={<profileImage/> }> </Route>
+          {/* <Route path="/student-application-for-company/:id" element={<StudentApplicatinCompany/> }> </Route> */}
           <Route path ="/" element={<AllStudents/>}></Route>      {/*normaly the home page featers at the end*/}
           </Routes>
         </div>
