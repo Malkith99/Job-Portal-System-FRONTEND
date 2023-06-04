1bfbd3528e6f0c363f848db238bc0b33e47f761b
Malkith99 <malkithamanda@gmail.com>
2023-06-04 09:05:53 +0530

    updated

--- src/components/pages/student/profileImage/profileImage.js
+++ src/components/pages/student/profileImage/profileImage.js
@@ -5,22 +5,37 @@ import axios from "axios";
 
 export default function ProfileImage(){
     const[profileImage,setImage]=useState("");
+    const[studentData,setData]=useState("");
+
+    function handleSave() {       // Perform any save operations here
+        sendImage();
+      }
+
+    function sendImage(){
+        const newStudent={
+            profileImage,
+        }
+        axios.put("http://localhost:1234/student/uploadImage/"+studentData?._id,newStudent ).then(res=>{
+            alert("Update Profile Image Successfully!");
+        }).catch(error=>console.error('Error:',error));
+    }
 
     useEffect(()=>{
         const fetchData =async()=>{
             const token = await  window.localStorage.getItem("token");
-            axios.post("http://localhost:1234/student/uplodImage",{},{
+            axios.post("http://localhost:1234/student/studentData",{},{
               headers:{
                 Authorization: `Bearer ${token}`,
               },
             })
             .then(res=>{ 
-                setImage(res.data.data.profileImage);
-            //   if (res.data.status=="ok"){
-            //     // setData(res.data.data);  
-            //   }else{
-            //     window.alert(res.data.error);
-            //   }
+               
+              if (res.data.status=="ok"){
+                setImage(res.data.profileImage);
+                setData(res.data.data);  
+              }else{
+                window.alert(res.data.error);
+              }
             })
             .catch(error=> console.error("Error: ",error));
           }
@@ -34,6 +49,10 @@ export default function ProfileImage(){
         <div className="container">
         <h1>Profile Image</h1>
         <h1>Profile</h1>
+
+        <button type="submit" className="btn btn-primary" onClick={handleSave}>
+                Save
+              </button>
         </div>
         </>
     )
