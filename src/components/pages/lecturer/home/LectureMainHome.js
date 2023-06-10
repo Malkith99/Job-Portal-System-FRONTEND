56aac12ef702ccb9b2956b85819d11799f9e43b6
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2023-06-10 23:03:32 +0530

    added lecturer side

---
+++ src/components/pages/lecturer/home/LectureMainHome.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Footer from "../../../footer/footer";
+import MainHeader from "../../../mainHeader/mainHeader";
+import LecturerHome from "./lecturerHome/LecturerHome";
+
+function LectureMainHome({ isLogedIn, onLogout }) {
+  const content = (
+    <>
+      <Link to="/lecture-home">Home</Link>
+      <Link to="/lecture-profile">Profile</Link>
+    </>
+  );
+  return (
+    <div>
+      <div>
+        <MainHeader
+          content={content}
+          isLogedIn={isLogedIn}
+          onLogout={onLogout}
+        />
+        <LecturerHome />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default LectureMainHome;
