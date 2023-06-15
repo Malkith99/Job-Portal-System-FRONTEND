0026e8b3ed07541f2b56d523f329ced3bd9744a7
Malkith99 <malkithamanda@gmail.com>
2023-06-15 16:12:08 +0530

    table of the admin page created

---
+++ src/components/pages/Admin/StudentData/StudentData.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+import axios from "axios";
+import "./StudentData.css";
+
+export default function AdminStudent() {
+  const [students, setStudents] = useState([]);
+  //   const [img, setImg] = useState("");
+  //   const [fname, setfName] = useState("");
+  //   const [lname, setlName] = useState("");
+  //   const [email, setEmail] = useState(0);
+
+  useEffect(() => {
+    function getStudents() {
+      axios
+        .get("http://localhost:1234/student/")
+        .then((res) => {
+          setStudents(res.data);
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    }
+    getStudents();
+  }, []);
+
+  return (
+    <>
+      <h1>Student Data</h1>
+      <div>
+        {/* {students.map((studentData) => (
+        <div>
+          <p>Name:{studentData.name}</p>
+          <p>Age:{studentData.age}</p>
+          <p>Gender:{studentData.gender}</p>
+        </div>
+        ))} */}
+
+        <table className="cart-table">
+          <thead>
+            <tr>
+              {/* <th>Profile Image</th> */}
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              {/* <th>Delete</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((studentData) => (
+              <tr key={studentData._id}>
+                {/* <td>
+                  <img
+                    src={"http//localhost:1234" + studentData.profileImage}
+                    alt=""
+                  />
+                </td> */}
+                <td>{studentData.fisrtName}</td>
+                <td>{studentData.lastName}</td>
+                <td>{studentData.email}</td>
+                {/* {/*  <td>{calculateItemTotalPrice(item.price, item.quantity)}</td>
+              <td>
+                <button onClick={() => removeItem(item._id)}>Delete</button>
+              </td>  */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
