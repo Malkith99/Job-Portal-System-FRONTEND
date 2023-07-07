3ee78ea0a8148bc444f7b1d8ac687f4d2ce297b5
Supun Thilakshana Dharmarathna <82252921+SupunTd@users.noreply.github.com>
2023-07-08 00:39:32 +0530

    Student Signin and Signup page connected to backend

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -52,7 +52,7 @@ function Home({ isLogedIn, onLogout }) {
           <div className="home-right-div p-4 w-100 d-flex flex-column">
             <h1 className="main-head">Are you?</h1>
             <Link
-              to="/student-register"
+              to="student-signIn"
               type="button"
               className="form-control text-white student-button btn btn-primary m-2 mb-4"
             >
