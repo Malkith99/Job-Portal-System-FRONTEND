5a862c839c142df4ef1c8e2fa7a1d9bd4d613722
Supun Thilakshana Dharmarathna <82252921+SupunTd@users.noreply.github.com>
2023-07-07 10:27:35 +0530

    Debus signin and signup page student side

--- src/components/mainHeader/profileLogo/profileLogo.js
+++ src/components/mainHeader/profileLogo/profileLogo.js
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Dropdown, DropdownButton } from "react-bootstrap";
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import "./profileLogo.css";
 
-function ProfileLogo(props) {
-  return (
-    <DropdownButton
-      id="profile-dropdown"
-      variant="light"
-      title={
-        <img
-          src="https://media.istockphoto.com/id/1338134336/photo/headshot-portrait-african-30s-man-smile-look-at-camera.jpg?b=1&s=170667a&w=0&k=20&c=j-oMdWCMLx5rIx-_W33o3q3aW9CiAWEvv9XrJQ3fTMU="
-          alt="Profile Logo"
-          className="profile-logo-img"
-        />
-      }
-      align="end"
-    >
-      <Dropdown.Item>
-        <Link to="/company-details">Profile</Link>
-      </Dropdown.Item>
-      <Dropdown.Divider />
-      <Dropdown.Item>
-        <Link to="/" onClick={props.onLogout}>
-          Logout
-        </Link>
-      </Dropdown.Item>
-    </DropdownButton>
-  );
+function ProfileLogo() {
+    const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem("token"));
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user") || "{}"));
+
+    // logout handle
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        console.log("User has been logged out");
+        setLoggedIn(false);
+        setUser(null);
+        window.location = "/";
+    };
+
+    return (
+        <div>
+            {loggedIn ? (
+                <DropdownButton id="profile-dropdown" variant="light" align="end" title={user.profilePhoto && (
+                    <img src={user.profilePhoto} alt="Profile Logo" className="profile-logo-img" />
+                )}>
+                    {/*
+                    <Dropdown.Item>
+                        <Link to="/students-details">Profile</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    */}
+
+                    <Dropdown.Item>
+                        <Link to="/" onClick={handleLogout}>
+                            Logout
+                        </Link>
+                    </Dropdown.Item>
+                </DropdownButton>
+            ) : (
+                <DropdownButton id="profile-dropdown" variant="light" align="end" title="Profile">
+                    <Dropdown.Item>
+                        <Link to="/home">Login</Link>
+                    </Dropdown.Item>
+                </DropdownButton>
+            )}
+        </div>
+    );
 }
 
 export default ProfileLogo;
---
+++ src/components/pages/student/StudentSignUp/SignUp/StudentSignUp.js
@@ -0,0 +1,149 @@
+import React,{ useState } from "react";
+import { Link } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import axios from "axios";
+
+
+styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+}));
+
+
+export default function StudentSignup() {
+
+
+    const [data, setData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        role: "student",
+    });
+
+    const [error, setError] = useState("");
+    const [msg, setMsg] = useState("");
+
+    const handleChange = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const url = "http://localhost:4000/api/users";
+            const response = await axios.post(url, data);
+            setMsg(response.data.message);
+            window.location = "/";
+        } catch (error) {
+            if (error.response && error.response.status >= 400 && error.response.status <= 500) {
+                setError(error.response.data.message);
+            }
+        }
+    };
+
+    return (
+        <>
+            <div>
+
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={1} align="center">
+                        <Grid item xs={12}>
+                            <Card sx={{ maxWidth: "50%", mt: 5, alignContent: "center", justifyItems: "flex-end" }}>
+                                <CardContent>
+                                    <Typography sx={{ fontSize: 18, fontWeight: "bold" }} color="Black" gutterBottom>
+                                        Create Your Account
+                                    </Typography>
+                                    <form onSubmit={handleSubmit}>
+                                        <TextField
+                                            type="text"
+                                            label="First Name"
+                                            name="firstName"
+                                            onChange={handleChange}
+                                            value={data.firstName}
+                                            required
+                                            fullWidth
+                                            margin="normal"
+                                        />
+                                        <TextField
+                                            type="text"
+                                            label="Last Name"
+                                            name="lastName"
+                                            onChange={handleChange}
+                                            value={data.lastName}
+                                            required
+                                            fullWidth
+                                            margin="normal"
+                                        />
+                                        <TextField
+                                            type="text"
+                                            label="Role"
+                                            name="role"
+                                            onChange={handleChange}
+                                            value="Student"
+                                            required
+                                            fullWidth
+                                            margin="normal"
+                                            disabled
+                                        />
+
+                                        <TextField
+                                            type="email"
+                                            label="Email"
+                                            name="email"
+                                            onChange={handleChange}
+                                            value={data.email}
+                                            required
+                                            fullWidth
+                                            margin="normal"
+                                        />
+
+                                        <TextField
+                                            type="password"
+                                            label="Password"
+                                            name="password"
+                                            onChange={handleChange}
+                                            value={data.password}
+                                            required
+                                            fullWidth
+                                            margin="normal"
+                                        />
+
+
+                                        {error && <div className="signup_error_msg">{error}</div>}
+                                        {msg && <div className="signup_success_msg">{msg}</div>}
+                                        <Button variant="contained" color="primary" type="submit">
+                                            Sign Up
+                                        </Button>
+                                    </form>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                    <hr />
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} align="right" mr={"25%"} sx={{ alignItems: "flex-end" }}>
+                            <div className="student">
+                                <div style={{ marginTop: "7px" }}>
+                                    <Link to="/student-signIn">Do you have an account? Click here to sign in.</Link>
+                                </div>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </div>
+        </>
+    );
+}
