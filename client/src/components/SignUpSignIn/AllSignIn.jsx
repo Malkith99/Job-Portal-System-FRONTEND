2d16f9ac1402b514837aa32f6b2ba496a9442d8b
Supun Thilakshana Dharmarathna <82252921+SupunTd@users.noreply.github.com>
2023-07-10 12:51:58 +0530

    Add all signin page and signup page

---
+++ client/src/components/SignUpSignIn/AllSignIn.jsx
@@ -0,0 +1,167 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Footer from "../footer/footer";
+import MainHeader from "../mainHeader/mainHeader";
+import axios from "axios";
+import allSignInPageImage from "../../images/im6.jpg";
+import "./AllSignin.css";
+function CompanyLogin() {
+    const [loggedIn] = useState(!!localStorage.getItem("token"));
+    const [user] = useState(JSON.parse(localStorage.getItem("user") || "{}"));
+
+    const [data, setData] = useState({ email: "", password: "" });
+    const [error, setError] = useState("");
+    const [msg, setMsg] = useState("");
+    const [showPopup, setShowPopup] = useState(false);
+
+    const handleChange = ({ currentTarget: input }) => {
+        setData({ ...data, [input.name]: input.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const url = "http://localhost:4000/api/auth";
+            const { data: res } = await axios.post(url, data);
+            setMsg(res.message);
+
+            localStorage.setItem("token", res.data);
+            localStorage.setItem("user", JSON.stringify(res.user));
+
+            // Create admin token if the user is an admin
+            if (res.user.email === "admin@gmail.com") {
+                localStorage.setItem("adminToken", res.data);
+                console.log("Admin has been Log In");
+                window.location = "/adminHome";
+            }
+
+            //check if user has carted
+            const user = JSON.parse(localStorage.getItem("user"));
+            console.log(user.firstName);
+            console.log(data);
+            console.log("User has been Log In");
+            console.log(`User ${data._id} has been login`);
+            setShowPopup(true);
+            window.location = "/";
+        } catch (error) {
+            if (
+                error.response &&
+                error.response.status >= 400 &&
+                error.response.status <= 500
+            ) {
+                setError(error.response.data.message);
+            }
+        }
+    };
+
+    axios.interceptors.request.use(
+        (config) => {
+            const token = localStorage.getItem("token");
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    );
+
+    return (
+        <div className="page-container">
+            <MainHeader
+                content={
+                    loggedIn ? (
+                        <>
+                            <Link to="/">Welcome, {user.firstName} {user.lastName}!</Link>
+                        </>
+                    ) : (
+                        <Link to="/">Home</Link>
+                    )
+                }
+            ></MainHeader>
+            <div className="ascontainer">
+                <div className="asrow justify-content-center">
+                    <div className="ascol-md-6">
+                        <div className="ascard-header text-center loginN">SignIn</div>
+
+
+                        <div className="ascard-body">
+                            <form className="asloginbox-content" onSubmit={handleSubmit}>
+                                <div className="asd-flex mb-3">
+                                    <div style={{ display: "flex", alignItems: "center" }}>
+                                        <img
+                                            src={allSignInPageImage}
+                                            alt="Your Image"
+                                            style={{ width: "400px", height: "400px", marginRight: "20px" }}
+                                        />
+                                    </div>
+
+                                    <div className="asflex-grow-1">
+                                        <div className="asform-group">
+                                            <label htmlFor="username" className="form-label">
+                                                <b style={{ fontSize: "20px" }}>Email:</b>
+                                            </label>
+                                            <input
+                                                className="form-control"
+                                                type="email"
+                                                placeholder="Email"
+                                                name="email"
+                                                onChange={handleChange}
+                                                value={data.email}
+                                                required
+                                            />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="password" className="form-label" style={{ fontSize: "20px" }}>
+                                                <b>Password:</b>
+                                            </label>
+                                            <input
+                                                className="form-control"
+                                                type="password"
+                                                placeholder="Password"
+                                                name="password"
+                                                onChange={handleChange}
+                                                value={data.password}
+                                                required
+                                            />
+                                        </div>
+                                        {error && <div className="login_error_msg">{error}</div>}
+                                        {msg && <div className="login_success_msg">{msg}</div>}
+                                    </div>
+                                </div>
+                                <div className="asmb-3 form-check" style={{ marginLeft: "22px" }}>
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                    <label className="asform-check-label" htmlFor="exampleCheck1">
+                                        Remember username
+                                    </label>
+                                </div>
+                                <button type="submit" className="asbtn-btn-primary" style={{backgroundColor: '#007bff', color: '#fff', padding: '10px 20px', border: 'none', borderRadius: '4px'}}>
+                                    Login
+                                </button>
+
+                                <div style={{ marginTop: "7px" }}>
+                                    <Link to="/home">Don't have an account? Click here to sign up.</Link>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {showPopup && (
+                <div className="popup">
+                    <div className="popup-content">
+            <span className="close" onClick={() => setShowPopup(false)}>
+              &times;
+            </span>
+                        {error && <div className="login_error_msg">{error}</div>}
+                        {msg && <div className="login_success_msg">{msg}</div>}
+                    </div>
+                </div>
+            )}
+            <Footer />
+        </div>
+    );
+}
+
+export default CompanyLogin;
---
+++ client/src/components/SignUpSignIn/SignInSignUp.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Footer from "../footer/footer";
+import MainHeader from "../mainHeader/mainHeader";
+import "./SignInSignUp.css";
+
+function SignInSignUp({ isLogedIn, onLogout }) {
+  const [showLecturerPassword, setShowLecturerPassword] = useState(false);
+  const [showCompanyPassword, setShowCompanyPassword] = useState(false);
+  const [lecturerPassword, setLecturerPassword] = useState("");
+  const [companyPassword, setCompanyPassword] = useState("");
+  const [error, setError] = useState(""); // State to store error message
+  const [msg, setMsg] = useState(""); // State to store success message
+  const [showPopup, setShowPopup] = useState(false); // State to control popup visibility
+
+  const content = (
+      <>
+        <Link to="/">Home</Link>
+      </>
+  );
+
+  const handleCompanyLinkClick = () => {
+    setShowCompanyPassword(true);
+    setShowLecturerPassword(false);
+
+    if (companyPassword === "1234") {
+      setMsg("Company password is correct!\nYou can setup or access your account now"); // Set success message
+      setError(""); // Clear the error message
+      setShowPopup(true); // Show the popup
+      window.location.href = "/company-signIn";
+    } else {
+      setError("Company password is incorrect!"); // Set error message
+      setMsg(""); // Clear the success message
+      setShowPopup(true); // Show the popup
+      console.log("Incorrect password for company");
+    }
+  };
+
+  const handleLecturerLinkClick = () => {
+    setShowLecturerPassword(true);
+    setShowCompanyPassword(false);
+
+    if (lecturerPassword === "1234") {
+      setMsg("Lecturer password is correct!\nYou can setup or access your account now");
+      // Set success message
+      setError(""); // Clear the error message
+      setShowPopup(true); // Show the popup
+      window.location.href = "/lecturer-login";
+    } else {
+      setError("Lecturer password is incorrect!"); // Set error message
+      setMsg(""); // Clear the success message
+      setShowPopup(true); // Show the popup
+      console.log("Incorrect password for lecturer");
+    }
+  };
+
+  const closePopup = () => {
+    setShowPopup(false); // Hide the popup
+    setError(""); // Clear the error message
+    setMsg(""); // Clear the success message
+  };
+
+  return (
+      <div className="s-page-container">
+        <MainHeader content={content} isLogedIn={isLogedIn} onLogout={onLogout} />
+        <div className="s-container">
+          <div className="d-flex flex-lg-row flex-column">
+            <div className="home-left-div p-4 w-100">
+              <h1 className="main-head">About</h1>
+              <p className="text-left">
+                The University of Ruhuna Faculty of Engineering Job Bank is a website that connects students,
+                alumni,
+                and faculty with job opportunities in the engineering field. The website provides a searchable database of jobs,
+                the ability to create a profile and upload a resume, the ability to receive job alerts,
+                and the ability to network with other engineers.
+                The University of Ruhuna Faculty of Engineering Job Bank is a valuable resource for anyone looking for a job in engineering.
+              </p>
+            </div>
+            <div className="home-right-div p-4 w-100 d-flex flex-column">
+              <h1 className="main-head">Are you?</h1>
+
+              <Link
+                  to="/student-signIn"
+                  type="button"
+                  className="form-control text-white student-button btn btn-primary m-2 mb-4"
+              >
+                A Student
+              </Link>
+//pw=1234
+              <button
+                  type="button"
+                  className="form-control text-white student-button btn btn-primary m-2 mb-4"
+                  onClick={handleLecturerLinkClick}
+              >
+                A Lecturer
+              </button>
+              {showLecturerPassword && (
+                  <>
+                    <input
+                        type="password"
+                        placeholder="Enter lecturer password"
+                        value={lecturerPassword}
+                        onChange={(e) => setLecturerPassword(e.target.value)}
+                        className="form-control m-2"
+                    />
+                  </>
+              )}
+
+              <button
+                  type="button"
+                  className="form-control text-white student-button btn btn-primary m-2"
+                  onClick={handleCompanyLinkClick}
+              >
+                A Company
+              </button>
+              {showCompanyPassword && (
+                  <>
+                    <input
+                        type="password"
+                        placeholder="Enter company password"
+                        value={companyPassword}
+                        onChange={(e) => setCompanyPassword(e.target.value)}
+                        className="form-control m-2"
+                    />
+                  </>
+              )}
+            </div>
+          </div>
+        </div>
+        {showPopup && (
+            <div className="popup">
+              <div className="popup-content">
+            <span className="close" onClick={closePopup}>
+              &times;
+            </span>
+                {error && <div className="login_error_msg">{error}</div>}
+                {msg && <div className="login_success_msg">{msg}</div>}
+              </div>
+            </div>
+        )}
+        <Footer />
+      </div>
+  );
+}
+
+export default SignInSignUp;
