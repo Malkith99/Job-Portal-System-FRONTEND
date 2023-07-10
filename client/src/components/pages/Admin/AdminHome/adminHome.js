9ac43d3ca567227f559f25302e040f0b89726bdb
Supun Thilakshana Dharmarathna <82252921+SupunTd@users.noreply.github.com>
2023-07-10 20:29:05 +0530

    graph error corrected

--- client/src/components/pages/Admin/AdminHome/adminHome.js
+++ client/src/components/pages/Admin/AdminHome/adminHome.js
@@ -1,18 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import MainHeader from "../../../mainHeader/mainHeader";
-import Footer from "../../../footer/footer";
-import "./adminHome.css"; // Import the CSS file
-import adminImage from "../../../../images/admin.png"
+import React, {useEffect, useState} from 'react';
+import MainHeader from '../../../mainHeader/mainHeader';
+import Footer from '../../../footer/footer';
+import './adminHome.css'; // Import the CSS file
+import adminImage from '../../../../images/admin.png';
+import {Bar} from "react-chartjs-2";
+
 
 function AdminHome() {
     const [users, setUsers] = useState([]);
     const [filteredUsers, setFilteredUsers] = useState([]);
     const [selectedRole, setSelectedRole] = useState('');
 
+
     useEffect(() => {
         fetchUsers().then(() => {});
         handleRoleFilter('');
+
     }, []);
+
+    useEffect(() => {
+
+    }, [filteredUsers]);
 
     const fetchUsers = async () => {
         try {
@@ -62,105 +70,148 @@ function AdminHome() {
 
 
 
+
+
+    // Generate data for the chart based on filtered users
+    const generateChartData = () => {
+        const roles = ['admin', 'lecturer', 'company', 'student'];
+        return {
+            labels: roles,
+            datasets: [
+                {
+                    label: 'Users by Role',
+                    backgroundColor: 'rgba(75,192,192,0.2)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(75,192,192,0.4)',
+                    hoverBorderColor: 'rgba(75,192,192,1)',
+                    data: roles.map((role) => filteredUsers.filter((user) => user.role === role).length),
+                },
+            ],
+        };
+    };
+
+
+
+
+
     return (
         <>
-            <MainHeader/>
-            <h1 className='sign' style={{display:"flex",justifyContent:"center",marginBottom:"50px",fontSize:"45px",color:"#004d99"}}><u>User List</u></h1>
-            <div className="admin-home-container container"
-            style={{display:"flex"}}> {/* Apply a container class */}
-                
-                <div style={{display:"flex",flex:"25%",justifyContent:"center",alignItems:"center"}}>
-                <img
-                        src={adminImage}
-                        className="image"
-                        alt="adminImage"
-                        
-                        
-                        />
-                </div>
-                
+            <MainHeader />
+            <h1 className="sign" style={{ display: 'flex', justifyContent: 'center', marginBottom: '50px', fontSize: '45px', color: '#004d99' }}>
+                <u>User List</u>
+            </h1>
+            <div className="admin-home-container container" style={{ display: 'flex' }}>
+                {/* Apply a container class */}
 
-                <div style={{flex:"75%",justifyContent:"center",marginLeft:"10%",marginTop:"6%"}}>
-                    <div className="filter-buttons" style={{display:"flex",justifyContent:"center",padding:"5px"}}>
+                <div style={{ display: 'flex', flex: '25%', justifyContent: 'center', alignItems: 'center' }}>
+                    <img src={adminImage} className="image" alt="adminImage" />
+                </div>
+
+                <div style={{ flex: '75%', justifyContent: 'center', marginLeft: '10%', marginTop: '6%' }}>
+                    <div className="filter-buttons" style={{ display: 'flex', justifyContent: 'center', padding: '5px' }}>
                         <button
                             onClick={() => handleRoleFilter('')}
                             className={selectedRole === '' ? 'active' : ''}
-                            style={{marginRight:"5px",width:"20%",height:"40px",background:"#000066",borderColor:"#000066",fontSize:"20px",color:"#f2f2f2"}}
+                            style={{ marginRight: '5px', width: '20%', height: '40px', background: '#000066', borderColor: '#000066', fontSize: '20px', color: '#f2f2f2' }}
                         >
                             All
                         </button>
                         <button
                             onClick={() => handleRoleFilter('admin')}
                             className={selectedRole === 'admin' ? 'active' : ''}
-                            style={{marginRight:"5px",width:"20%",height:"40px",background:"#000066",borderColor:"#000066",fontSize:"20px",color:"#f2f2f2"}}
+                            style={{ marginRight: '5px', width: '20%', height: '40px', background: '#000066', borderColor: '#000066', fontSize: '20px', color: '#f2f2f2' }}
                         >
                             Admin
                         </button>
                         <button
                             onClick={() => handleRoleFilter('company')}
                             className={selectedRole === 'company' ? 'active' : ''}
-                            style={{marginRight:"5px",width:"20%",height:"40px",background:"#000066",borderColor:"#000066",fontSize:"20px",color:"#f2f2f2"}}
+                            style={{ marginRight: '5px', width: '20%', height: '40px', background: '#000066', borderColor: '#000066', fontSize: '20px', color: '#f2f2f2' }}
                         >
                             Company
                         </button>
                         <button
                             onClick={() => handleRoleFilter('student')}
                             className={selectedRole === 'student' ? 'active' : ''}
-                            style={{marginRight:"5px",width:"20%",height:"40px",background:"#000066",borderColor:"#000066",fontSize:"20px",color:"#f2f2f2"}}
+                            style={{ marginRight: '5px', width: '20%', height: '40px', background: '#000066', borderColor: '#000066', fontSize: '20px', color: '#f2f2f2' }}
                         >
                             Student
                         </button>
                         <button
                             onClick={() => handleRoleFilter('lecturer')}
                             className={selectedRole === 'lecturer' ? 'active' : ''}
-                            style={{marginRight:"5px",width:"20%",height:"40px",background:"#000066",borderColor:"#000066",fontSize:"20px",color:"#f2f2f2"}}
+                            style={{ marginRight: '5px', width: '20%', height: '40px', background: '#000066', borderColor: '#000066', fontSize: '20px', color: '#f2f2f2' }}
                         >
                             Lecturer
                         </button>
                     </div>
                     {selectedRole && (
-                        <h2 style={{ display: "flex", justifyContent: "center", margin: "10px" }}>
+                        <h2 style={{ display: 'flex', justifyContent: 'center', margin: '10px' }}>
                             Showing {countUsersByRole(selectedRole)} {selectedRole} users
                         </h2>
                     )}
-                {filteredUsers.length > 0 ? (
-                    <table className="user-table"> {/* Apply a class to the table */}
-                        <thead>
-                        <tr>
-                            <th>Photo</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {filteredUsers.map((user) => (
-                            <tr key={user._id}>
-                                <td>
-                                    <img src={user.profilePhoto} alt={user.name} className="user-photo" /> {/* Apply a class to the user photo */}
-                                </td>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.role}</td>
-                                <td>
-                                    <button onClick={() => handleEdit(user._id)}>Edit</button>
-                                    <button onClick={() => handleDelete(user._id)}>Delete</button>
-                                </td>
+                    {filteredUsers.length > 0 ? (
+                        <table className="user-table">
+                            {/* Apply a class to the table */}
+                            <thead>
+                            <tr>
+                                <th>Photo</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Actions</th>
                             </tr>
-                        ))}
-                        </tbody>
-                    </table>
-                ) : (
-                    <p style={{display:"flex",justifyContent:"center",margin:"10px",fontSize:"20px"}}>No users found</p>
-                )}
-                    
+                            </thead>
+                            <tbody>
+                            {filteredUsers.map((user) => (
+                                <tr key={user._id}>
+                                    <td>
+                                        <img src={user.profilePhoto} alt={user.name} className="user-photo" />
+                                    </td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.role}</td>
+                                    <td>
+                                        <button onClick={() => handleEdit(user._id)}>Edit</button>
+                                        <button onClick={() => handleDelete(user._id)}>Delete</button>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    ) : (
+                        <p style={{ display: 'flex', justifyContent: 'center', margin: '10px', fontSize: '20px' }}>No users found</p>
+                    )}
                 </div>
-
-
-
-
             </div>
+            {/*
+            //graph error give white error message
+
+            <div style={{ width: '500px', height: '400px' }}>
+                <Bar
+                    data={generateChartData()}
+                    options={{
+                        maintainAspectRatio: false,
+                        scales: {
+                            y: {
+                                beginAtZero: true,
+                                ticks: {
+                                    precision: 0,
+                                },
+                            },
+                        },
+                        plugins: {
+                            legend: {
+                                display: true,
+                                position: 'bottom',
+                            },
+                        },
+                    }}
+                />
+            </div>
+*/}
+
             <Footer />
         </>
     );
