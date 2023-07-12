c09b8ef60525be9ec607562c33e7ec06e01ac127
Supun Thilakshana Dharmarathna <82252921+SupunTd@users.noreply.github.com>
2023-07-12 11:23:07 +0530

    student profile data updated

--- src/components/pages/student/profile/ProfilePopup.js
+++ src/components/pages/student/profile/ProfilePopup.js
@@ -2,35 +2,45 @@ import React, { useState } from "react";
 
 function ProfilePic(props) {
   const [file, setFile] = useState(
-    "https://t4.ftcdn.net/jpg/00/65/77/27/360_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
+      "https://t4.ftcdn.net/jpg/00/65/77/27/360_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
   );
-  function handleChange(e) {
-    console.log(e.target.files);
-    setFile(URL.createObjectURL(e.target.files[0]));
+
+  function handleSave() {
+    props.sendData(file);
+    alert("Save successful. Please close the window manually.");
   }
 
-  //const { setOpenPopup } = props;
-  function handleSave() {}
+  function handleFileChange(e) {
+    const selectedFile = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      const base64String = reader.result;
+      setFile(base64String);
+    };
+
+    if (selectedFile) {
+      reader.readAsDataURL(selectedFile);
+    }
+  }
 
   return (
-    <div
-      className="container1-flex-item1 text-center"
-      style={{ display: "flex", flexDirection: "column" }}
-    >
-      <img className="profile-photo" src={file} alt="Profile Photo" />
-      <label className="label-title">Profile Photo</label>
-      <div className="file-input-div">
-        <input
-          type="file"
-          class="file-input-field form-control"
-          onChange={handleChange}
-        />
+      <div className="container1-flex-item1 text-center">
+        <img className="profile-photo" src={file} alt="Profile Photo" />
+        <label className="label-title">Profile Photo</label>
+        <div className="file-input-div">
+          <input
+              type="file"
+              className="file-input-field form-control"
+              onChange={handleFileChange}
+          />
+        </div>
+        <div style={{ marginBottom: "1rem" }}></div>
+        <button type="button" className="btn btn-primary" onClick={handleSave}>
+          Save
+        </button>
       </div>
-      <div style={{ marginBottom: "1rem" }}></div>
-      <button type="Submit" class="btn btn-primary" onClick={handleSave}>
-        Save
-      </button>
-    </div>
   );
 }
+
 export default ProfilePic;
