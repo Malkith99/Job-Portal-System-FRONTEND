c88286f3bdfdb7b070e4a11a1891a66c4e55d736
Supun Thilakshana Dharmarathna <82252921+SupunTd@users.noreply.github.com>
2023-07-28 09:42:20 +0530

    code update

--- src/components/pages/student/home/StudentHome.js
+++ src/components/pages/student/home/StudentHome.js
@@ -10,81 +10,62 @@ import { IconButton } from '@mui/material';
 import Card from "../../student/home/studentHome/Feed";
 
 export default function StudentHome({ isLogedIn, onLogout }) {
-  const [searchQuery, setSearchQuery] = useState('');
-  const content = (
-    <>
-      <Link to="/student-profile">Profile</Link>
-      <Link to="/student-applications">My Applications</Link>
-    </>
-  );
-  const [token, setToken] = useState('');
-  useEffect(() => {
-    setToken(window.localStorage.getItem('token'));
-  }, []);
-  useEffect(() => {
-    console.log(token);
-  }, [token]);
-  
-  return (
-      <div>
-      <MainHeader content={content} isLogedIn={isLogedIn} onLogout={onLogout} />
-      <div className="page">
-        <div className="strip-section">
-     
-          <div className="strip-content">
-            <h2 className="section-title text-style">
-              Find the Job <br /> that is perfect for you
-            </h2>
-          </div>
-          <div className="searchBar">
-            <TextField
-              id="search"
-              label="Search Company Name"
-              variant="outlined"
-              size="small"
-              style={{marginLeft:150,marginTop:50}}
-              value={searchQuery}
-              onChange={(event) => setSearchQuery(event.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <IconButton color="inherit">
-                    <SearchIcon />
-                  </IconButton>
-                ),
-              }}
-            />
-          </div>
-          <img src={stripImage} alt="Strip" className="strip-image" />
-        </div>
-        <div className="feeds-section" style={{height:"auto",padding:"25px"}}>
-          
-          <Card/>
-        </div>
-        <div className="paragraphs-section">
-          {/* <p>Paragraph 1</p>
-          <p>Paragraph 2</p>
-          <p>Paragraph 3</p> */}
-        </div>
-       </div>
-       <Footer />
-     </div> 
-  );
-}
+    const [searchQuery, setSearchQuery] = useState('');
+    const content = (
+        <>
+            <Link to="/student-profile">Profile</Link>
+            <Link to="/student-applications">My Applications</Link>
+        </>
+    );
+    const [token, setToken] = useState('');
+    useEffect(() => {
+        setToken(window.localStorage.getItem('token'));
+    }, []);
+    useEffect(() => {
+        console.log(token);
+    }, [token]);
 
-  
-        
-/* /*         <div>            
-            <h1>Student Home</h1>
-            <div className="student">
-              <Link to="/student-profile">
-                <Button variant="contained">
-                  profile
-                </Button>
-              </Link>
-              <Link to="/student-application">
-                <Button variant="contained">
-                  My Applications
-                </Button>
-              </Link>
+    return (
+        <div>
+            <MainHeader content={content} isLogedIn={isLogedIn} onLogout={onLogout} />
+            <div className="page">
+                <div class="strip-section">
+                    <div class="centered-content">
+                        <h2 class="section-title text-style">
+                            Find the Job  that is perfect for you
+                        </h2>
+                    </div>
+                    <div className="searchBar">
+                        <TextField
+                            id="search"
+                            label="Search Company Name"
+                            variant="outlined"
+                            size="small"
+                            style={{marginLeft:150,marginTop:50}}
+                            value={searchQuery}
+                            onChange={(event) => setSearchQuery(event.target.value)}
+                            InputProps={{
+                                endAdornment: (
+                                    <IconButton color="inherit">
+                                        <SearchIcon />
+                                    </IconButton>
+                                ),
+                            }}
+                        />
+
+                    </div>
+                    <div class="right-column">
+                        <img src={stripImage} alt="Strip" class="strip-image" />
+                    </div>
+                </div>
+                <div className="feeds-section" style={{height:"auto",padding:"25px",marginTop:"100px"}}>
+
+                    <Card/>
+                </div>
+                <div className="paragraphs-section">
+                </div>
             </div>
-        </div> */
+            <Footer />
+        </div>
+    );
+}
--- src/components/pages/student/home/studentHome/Feed.js
+++ src/components/pages/student/home/studentHome/Feed.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './Feed.css';
-import { useNavigate } from 'react-router-dom';
-import {URL} from "../../../../../env";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Feed.css";
+import { useNavigate } from "react-router-dom";
+import { URL } from "../../../../../env";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
 export default function Feed() {
     const navigate = useNavigate();
     const [jobpool, setJobpool] = useState([]);
@@ -11,19 +13,19 @@ export default function Feed() {
     useEffect(() => {
         const fetchJobVacancies = async () => {
             try {
-                const response = await axios.get(URL +'/api/vacancies');
+                const response = await axios.get(URL + "/api/vacancies");
                 setJobpool(response.data);
             } catch (error) {
-                console.error('Failed to fetch job vacancies:', error);
+                console.error("Failed to fetch job vacancies:", error);
             }
         };
 
         const fetchCompanies = async () => {
             try {
-                const response = await axios.get(URL +'/api/vacancies');
+                const response = await axios.get(URL + "/api/vacancies");
                 setCompanies(response.data);
             } catch (error) {
-                console.error('Failed to fetch companies:', error);
+                console.error("Failed to fetch companies:", error);
             }
         };
 
@@ -32,7 +34,7 @@ export default function Feed() {
     }, []);
 
     return (
-        <div className='wrapper'>
+        <div className="wrapper">
             {jobpool.map((vacancy) => {
                 const { _id } = vacancy;
                 const items = vacancy.items;
@@ -40,31 +42,112 @@ export default function Feed() {
                 return (
                     <React.Fragment key={_id}>
                         {items.map((item, index) => {
-                            const { jobPosition, salary,dueDate, flyer,jobDescription, companyId } = item;
+                            const {
+                                jobPosition,
+                                salary,
+                                dueDate,
+                                flyer,
+                                jobDescription,
+                                companyId,
+
+
+                            } = item;
 
                             // Find the corresponding company
                             const company = companies.find((c) => c._id === companyId);
 
                             return (
-                                <div key={`${_id}-${index}`} className='card'>
-                                    <div className='imagestyle'>
-                                        {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-                                        <img alt="Card Image" src={`data:image/jpeg;base64/${flyer}`} />
+                                <div>
+                                    <div key={`${_id}-${index}`} className="card">
+                                        <div className="imagestyle">
+                                            {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+                                            <img
+                                                alt="Card Image"
+                                                src={`data:image/jpeg;base64/${flyer}`}
+                                            />
+                                        </div>
+                                        <h3
+                                            className={`card_title ${
+                                                jobPosition.length > 25 ? "card_title--small" : ""
+                                            }`}
+                                        >
+                                            {jobPosition}
+                                        </h3>
+                                        <p className="salary">Salary: {salary}</p>
+                                        <p className="due-date">Due Date: {dueDate}</p>
+                                        <p className="due-date">Description: {jobDescription}</p>
+                                        {company && (
+                                            <p className="company-name">
+                                                Company: {company.firstName}
+                                            </p>
+                                        )}
+                                        <div className="button-section">
+                                            <button
+                                                className="button-section"
+                                                onClick={() => {
+                                                    navigate(`/company-vacancy-view-student/${item._id}`);
+                                                }}
+                                            >
+                                                View
+                                            </button>
+                                        </div>
                                     </div>
-                                    <h3 className={`card_title ${jobPosition.length > 25 ? 'card_title--small' : ''}`}>
-                                        {jobPosition}
-                                    </h3>
-                                    <p className="due-date">Description: {jobDescription}</p>
-                                    <p className="salary">Salary: {salary}</p>
-                                    <p className="due-date">Due Date: {dueDate}</p>
-                                    {company && <p className="company-name">Company: {company.firstName}</p>}
-                                    <div className='button-section'>
-                                        <button className='button-section' onClick={() => {
-                                                navigate(`/company-vacancy-view-student/${item._id}`);
-                                            }}>
-                                            View
-                                        </button>
-                                    </div>
+
+
+
+                                    <div style={{ marginTop: "30px" }}></div>
+                                    <Card key={`${_id}-${index}`} className="custom-card1">
+                                        <div className="image-box">
+                                            <img
+                                                alt="Card Image"
+                                                src={`data:image/jpeg;base64/${flyer}`}
+                                            />
+                                        </div>
+                                        <div >
+                                            <h3
+                                                className="title-box"
+                                            >
+                                                Mobitel
+                                            </h3>
+                                        </div>
+                                        <div className="Branch-box">
+                                            <h5>Colombo,LK</h5>
+                                        </div>
+                                        <div className="para-items">
+
+                                            <div className="para-box">
+                                                <p>Full-time </p>
+                                            </div>
+                                            <div className="para-box">
+                                                <p>Online </p>
+                                            </div>
+                                        </div>
+                                        <div className="job-pos-box">
+                                            <h2>
+                                                Full-Stack Developer
+                                            </h2>
+                                        </div>
+                                        <div className="intro">
+                                            <p>
+                                                Description: {jobDescription}
+                                            </p>
+                                        </div>
+                                        <div className="para-items2" >
+                                            <div>
+                                                <p className="salary">$ {salary}/Month</p>
+
+                                            </div>
+                                            <div >
+                                                <button className="button-style"
+                                                        onClick={() => {
+                                                            navigate(`/company-vacancy-view-student/${item._id}`);
+                                                        }}
+                                                >
+                                                    Apply Now
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </Card>
                                 </div>
                             );
                         })}
