c3b88590aa035fc9cc9e72e92b377a6a80a1a023
Supun Thilakshana Dharmarathna <82252921+SupunTd@users.noreply.github.com>
2023-08-01 13:15:08 +0530

    remove token

--- src/components/mainHeader/profileLogo/profileLogo.js
+++ src/components/mainHeader/profileLogo/profileLogo.js
@@ -10,6 +10,7 @@ function ProfileLogo() {
     // logout handle
     const handleLogout = () => {
         localStorage.removeItem("token");
+        localStorage.removeItem("jbusers");
         console.log("User has been logged out");
         setLoggedIn(false);
         setUser(null);
