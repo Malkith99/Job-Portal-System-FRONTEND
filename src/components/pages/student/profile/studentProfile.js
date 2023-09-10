4bbd5a062b4ed74b6f293e543e56376c14dda09c
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2023-09-10 18:37:55 +0530

    student profile fixing

--- src/components/pages/student/profile/studentProfile.js
+++ src/components/pages/student/profile/studentProfile.js
@@ -170,7 +170,7 @@ export default function Profile() {
   };
 
   return (
-      <div   className="container progress-div"
+      <div className="container progress-div"
       style={{
         display: "flex",
         // flexDirection: "column",
@@ -183,7 +183,8 @@ export default function Profile() {
       }}
     >
         <form action="/student-home">
-          <h4 className="sub-headings">Personal info: </h4>
+          <h4 className="sub-headings"
+          style={{marginTop:"2%",fontWeight:"bolder"}}>Personal info: </h4>
          
        <div >    
           <div
@@ -223,14 +224,15 @@ export default function Profile() {
 </div>
 
 
-            </div>
+  </div>
 </div>
 {/* <div className="flex-container1"> */}
-<Card className="carda " style={{marginBottom:"25px",marginTop:"25px",padding:"25px"}}>
-            <div className="container1" style={{padding:10}}>
+<Card className="carda " style={{padding:"25px",boxShadow:"none"}}>
+            <div className="container1">
               <div className="sub-flex-container">
                 <div className="sub-flex-item1">
-                  <label className="label-title">Name</label>
+                  <label className="label-title">Full Name</label>
+                  <span className="asterisk-mark">*</span>
                 </div>
                 <div className="sub-flex-item2">
                   <div className="input-filed">
@@ -246,7 +248,7 @@ export default function Profile() {
                         required
                     ></input>
                   </div>
-                  <div className="text-center">
+                  <div className="text-center" style={{marginTop:"-20px"}}>
                     <label className="hint-title">
                       First Name<span className="asterisk-mark">*</span>
                     </label>
@@ -265,7 +267,7 @@ export default function Profile() {
                         disabled={disabled}
                     ></input>
                   </div>
-                  <div className="text-center">
+                  <div className="text-center" style={{marginTop:"-20px"}}>
                     <label className="hint-title" >Middle Name</label>
                   </div>
                 </div>
@@ -283,7 +285,7 @@ export default function Profile() {
                         disabled={disabled}
                     ></input>
                   </div>
-                  <div className="text-center">
+                  <div className="text-center" style={{marginTop:"-20px"}}>
                     <label className="hint-title">
                       Last Name<span className="asterisk-mark">*</span>
                     </label>
@@ -312,10 +314,6 @@ export default function Profile() {
                     ></input>
                   </div>
                 </div>
-                <div className="sub-flex-item2"></div>
-                <div className="sub-flex-item2"></div>
-              </div>
-              <div className="sub-flex-container">
                 <div className="sub-flex-item1">
                   <label className="label-title">
                     Date of Birth
@@ -337,9 +335,14 @@ export default function Profile() {
                     ></input>
                   </div>
                 </div>
-                <div className="sub-flex-item2"></div>
-                <div className="sub-flex-item2"></div>
+                {/* <div className="sub-flex-item2"></div>
+                <div className="sub-flex-item2"></div> */}
               </div>
+              {/* <div className="sub-flex-container">
+                
+                <div className="sub-flex-item2"></div>
+                <div className="sub-flex-item2"></div>
+              </div> */}
               <div className="sub-flex-container">
                 <div className="sub-flex-item1">
                   <label className="label-title">
@@ -367,10 +370,6 @@ export default function Profile() {
                     </select>
                   </div>
                 </div>
-                <div className="sub-flex-item2"></div>
-                <div className="sub-flex-item2"></div>
-              </div>
-              <div className="sub-flex-container">
                 <div className="sub-flex-item1">
                   <label className="label-title">
                     Phone Number
@@ -391,15 +390,18 @@ export default function Profile() {
                         disabled={disabled}
                     ></input>
                   </div>
-                  <div className="text-center">
+                  {/* <div className="text-center">
                     <label className="hint-title">
                       Phone Number 1<span className="asterisk-mark">*</span>
                     </label>
-                  </div>
+                  </div> */}
                 </div>
-
-
               </div>
+              {/* <div className="sub-flex-container">
+                
+
+
+              </div> */}
               {/*
               <div className="sub-flex-container">
                 <div className="sub-flex-item1">
@@ -449,8 +451,10 @@ export default function Profile() {
 
           )}
   </Card>
-          <h4 className="sub-headings">Academic Details: </h4>
-        <Card className="carda " style={{marginBottom:"25px",marginTop:"25px",padding:"25px"}}>    <div className="">
+          <h4 className="sub-headings" style={{marginTop:"2%",fontWeight:"bolder"}}
+          >Academic Details: </h4>
+        <Card className="carda " style={{padding:"25px",boxShadow:"none"}}>    
+      
            
             <div className="flex-container2">
               <div className="container2-flex-item">
@@ -553,9 +557,6 @@ export default function Profile() {
                   </div>
                 </div>
               </div>
-              <div className="container2-flex-item"></div>
-            </div>
-            <div className="flex-container2">
               <div className="container2-flex-item">
                 <div className="container2-flex-item-sub-item1">
                   <label className="label-title">
@@ -586,6 +587,10 @@ export default function Profile() {
                   </div>
                 </div>
               </div>
+              {/* <div className="container2-flex-item"></div> */}
+            </div>
+            <div className="flex-container2">
+
               <div className="container2-flex-item"></div>
             </div>
             <div className="flex-container2">
@@ -593,6 +598,7 @@ export default function Profile() {
                 <div className="container2-flex-item-sub-item3">
                   <label className="label-title">Projects</label>
                 </div>
+                
                 <div className="container2-flex-item-sub-item4">
                   <div className="input-filed input-filed-cls">
                   <textarea
@@ -609,7 +615,7 @@ export default function Profile() {
               </div>
             </div>
             
-          </div>
+          
           <button
               type="button"
               className="btn btn-primary"
@@ -628,37 +634,43 @@ export default function Profile() {
               </Popup>
 
           )}
-</Card>
-          <h4 className="sub-headings">Reference: </h4>
-          <Card className="carda " style={{marginBottom:"25px",marginTop:"25px",padding:"25px"}}>
-          <div className="">
+          </Card>
+          <h4 className="sub-headings"
+          style={{marginTop:"2%",fontWeight:"bolder"}}>Reference: </h4>
+          <Card className="carda " style={{padding:"25px",boxShadow:"none"}}>
+          
             <div className="flex-container2">
               <div className="container2-flex-item">
-                <div className="container2-flex-item-sub-item3">
+                {/* <div className="container2-flex-item-sub-item3">
                   <label className="label-title">References</label>
-                </div>
+                </div> */}
                 <div
                     className="container1-flex-item1 "
                     style={{ display: "flex", flexDirection: "column" }}
                 >
                   <img className="profile-photo-2" src={`data:image/jpeg;base64/${rdp}`} alt="Profile Photo" />
-                  <label form="profilePhoto" className="">
-                    <span className="asterisk-mark">*</span> Profile Photo
-                  </label>
+                  
 
                 </div>
-                <div className="container2-flex-item-sub-item4">
-                  <div className="input-filed input-filed-cls">
 
+                <div className="container2-flex-item-sub-item4">
+                  
+                  <div className="container2-flex-item-sub-item1">
+                    <label form="profilePhoto" className="">
+                      Refree Name<span className="asterisk-mark">*</span> 
+                    </label>
+                    <div className="input-filed input-filed-cls">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                        value={rfName +" "+ rlName}
+                        disabled={disabled}
+                    ></input>
 
                   </div>
-                  <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Name"
-                      value={rfName +" "+ rlName}
-                      disabled={disabled}
-                  ></input>
+                  </div>
+                  
                 </div>
                 {/* <div>
             {disabled ? (
@@ -682,7 +694,7 @@ export default function Profile() {
               </div>
             </div>
 
-          </div>
+          
           <button
               type="button"
               className="btn btn-primary"
