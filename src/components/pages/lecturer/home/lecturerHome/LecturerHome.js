e11dcc7c47d3e08ad6d3c2c9091aa5ccc4eb8953
Malkith99 <malkithamanda@gmail.com>
2023-09-11 19:23:22 +0530

    3 columns are done in Lecture_Home

--- src/components/pages/lecturer/home/lecturerHome/LecturerHome.js
+++ src/components/pages/lecturer/home/lecturerHome/LecturerHome.js
@@ -29,72 +29,87 @@ function RecommendationComponent() {
     setLecturerIdToMatch(idToMatch);
 
     // Fetch recommendations from the server
-    axios.get(URL+'/api/recommendations')
-        .then(response => {
-          // Filter recommendations based on lecturerIdToMatch
-          const filteredRecommendations = response.data.filter(recommendation => recommendation.lecturerId === idToMatch);
-          //setRecommendations(filteredRecommendations);
+    // axios.get(URL+'/api/recommendations')
+    //     .then(response => {
+    //       // Filter recommendations based on lecturerIdToMatch
+    //       const filteredRecommendations = response.data.filter(recommendation => recommendation.lecturerId === idToMatch);
+    //       //setRecommendations(filteredRecommendations);
 
-          // Extract student IDs from filtered recommendations
-        const studentIds = filteredRecommendations.map(recommendation => recommendation.studentId);
+    //       // Extract student IDs from filtered recommendations
+    //     const studentIds = filteredRecommendations.map(recommendation => recommendation.studentId);
 
-        // Extract vacancy IDs from filtered recommendations
-        const vacancyIds = filteredRecommendations.map(recommendation => recommendation.vacancyId);
+    //     // Extract vacancy IDs from filtered recommendations
+    //     const vacancyIds = filteredRecommendations.map(recommendation => recommendation.vacancyId);
 
-        // Fetch student data for each student ID
-        const studentPromises = studentIds.map(studentId =>
-          axios.get(`${URL}/api/users/${studentId}`)
-        );
+    //     // Fetch student data for each student ID
+    //     const studentPromises = studentIds.map(studentId =>
+    //       axios.get(`${URL}/api/users/${studentId}`)
+    //     );
        
-        // Fetch vacancy data for each vacancy ID (similar to your previous code)
-        const vacancyPromises = vacancyIds.map(vacancyId =>
-            axios.get(`${URL}/api/vacancies/${companyId}/${vacancyId}`)
-          );
+    //     // Wait for all promises to complete
+    //     Promise.all(studentPromises)
+    //       .then(studentResponses => {
+    //         // Extract student data from responses
+    //         const studentsData = studentResponses.map(response => response.data.user);
 
-        // Wait for all promises to complete
-        // Promise.all(studentPromises)
-        //   .then(studentResponses => {
-        //     // Extract student data from responses
-        //     const studentsData = studentResponses.map(response => response.data.user);
+    //         // Combine student data with filtered recommendations
+    //         const updatedRecommendations = filteredRecommendations.map((recommendation, index) => ({
+    //           ...recommendation,
+    //           studentData: studentsData[index], // Attach student data to recommendations
+    //         }));
 
-        //     // Combine student data with filtered recommendations
-        //     const updatedRecommendations = filteredRecommendations.map((recommendation, index) => ({
-        //       ...recommendation,
-        //       studentData: studentsData[index], // Attach student data to recommendations
-        //     }));
+    //         setRecommendations(updatedRecommendations);
+    //       })
+    //       .catch(error => {
+    //         console.error('Error fetching student information:', error);
+    //       });
+        
+    //     })
+    //     .catch(error => {
+    //       console.error('Error fetching recommendations:', error);
+    //     });
+// Assuming you have already fetched recommendations
+axios.get(URL + '/api/recommendations')
+  .then(response => {
+    // Filter recommendations based on lecturerIdToMatch
+    const filteredRecommendations = response.data.filter(recommendation => recommendation.lecturerId === idToMatch);
 
-        //     setRecommendations(updatedRecommendations);
-        //   })
-        //   .catch(error => {
-        //     console.error('Error fetching student information:', error);
-        //   });
-        Promise.all([...studentPromises, ...vacancyPromises])
-          .then(responses => {
-            const studentResponses = responses.slice(0, studentPromises.length);
-            const vacancyResponses = responses.slice(studentPromises.length);
+    // Fetch data for each recommendation
+    const recommendationPromises = filteredRecommendations.map(recommendation => {
+      const studentId = recommendation.studentId;
+      const companyId = recommendation.companyId;
+      const vacancyId = recommendation.vacancyId;
 
-            // Extract student data from responses (similar to your previous code)
-            const studentsData = studentResponses.map(response => response.data.user);
 
-            // Extract vacancy data from responses
-            const vacanciesData = vacancyResponses.map(response => response.data);
+      const studentPromise = axios.get(`${URL}/api/users/${studentId}`);
+      const companyPromise = axios.get(`${URL}/api/users/${companyId}`);
+      const vacancyPromise = axios.get(`${URL}/api/vacancies/${companyId}/${vacancyId}`);
 
-            // Combine student and vacancy data with filtered recommendations
-            const updatedRecommendations = filteredRecommendations.map((recommendation, index) => ({
-              ...recommendation,
-              studentData: studentsData[index], // Attach student data to recommendations
-              vacancyData: vacanciesData[index], // Attach vacancy data to recommendations
-            }));
+      return Promise.all([studentPromise, companyPromise, vacancyPromise])
+        .then(responses => ({
+          recommendation,
+          studentData: responses[0].data.user,
+          companyData: responses[1].data.user,
+          vacancyData: responses[2].data,
+        }));
+        console.log("recommendation.vacancyData", recommendation.vacancyData);
+        console.log("recommendation.vacancyData.items", recommendation.vacancyData.items);
 
-            setRecommendations(updatedRecommendations);
-          })
-          .catch(error => {
-            console.error('Error fetching information:', error);
-          });
-        })
-        .catch(error => {
-          console.error('Error fetching recommendations:', error);
-        });
+    });
+
+    // Wait for all promises to complete
+    Promise.all(recommendationPromises)
+      .then(updatedRecommendations => {
+        setRecommendations(updatedRecommendations);
+      })
+      .catch(error => {
+        console.error('Error fetching information:', error);
+      });
+  })
+  .catch(error => {
+    console.error('Error fetching recommendations:', error);
+  });
+
   }, []);
 
     const handleViewButtonClick = (vacancyId, studentId, companyId) => {
@@ -109,7 +124,8 @@ function RecommendationComponent() {
 
         window.location.href = `student-application-for-lecturer/${vacancyId}&${studentId}&${companyId}`;
     };
-    console.log(students);
+  
+  
   
     return (
         <div className='container'>
@@ -121,7 +137,7 @@ function RecommendationComponent() {
                         <TableRow>
                             <TableCell style={{textAlign:"center",fontWeight:"bold",fontSize:"18px",color:"white"}}>Student Name</TableCell>
                             <TableCell style={{textAlign:"center",fontWeight:"bold",fontSize:"18px",color:"white"}}>Company Name</TableCell>
-                            <TableCell style={{textAlign:"center",fontWeight:"bold",fontSize:"18px",color:"white"}}>Vacancy </TableCell>
+                            <TableCell style={{textAlign:"center",fontWeight:"bold",fontSize:"18px",color:"white"}}>Vacany Type </TableCell>
                             <TableCell style={{textAlign:"center",fontWeight:"bold",fontSize:"18px",color:"white"}}>Comment</TableCell>
                             <TableCell style={{textAlign:"center",fontWeight:"bold",fontSize:"18px",color:"white"}}>View</TableCell>
                         </TableRow>
@@ -132,9 +148,9 @@ function RecommendationComponent() {
                                 <TableCell>{recommendation.studentData.firstName} {recommendation.studentData.lastName}</TableCell>
                                 {/* <TableCell>{recommendation.vacancyData.items[0].companyName}</TableCell>
                                 <TableCell>{recommendation.vacancyData.items[0].jobPosition}</TableCell> */}
-                                <TableCell>{recommendation.companyId}</TableCell>
-                                <TableCell>{recommendation.vacancyId}</TableCell>
-                                <TableCell>{recommendation.comment}</TableCell>
+                                <TableCell>{recommendation.companyData.firstName} {recommendation.companyData.lastName}</TableCell>
+                                <TableCell>{recommendation.vacancyData.jobPosition}</TableCell>
+                                <TableCell>{recommendation.studentId}</TableCell>
                                 <TableCell>
                                     <Button
                                         variant="outlined"
@@ -154,7 +170,6 @@ function RecommendationComponent() {
                     </TableBody>
                 </Table>
             </TableContainer>
-
         </div>
     );
 }
